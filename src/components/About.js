710379312b2f493e97a49e3c37c7c19a3249c7c1
ThSchweers <th.schweers@googlemail.com>
2021-08-10 08:05:43 +0200

    Add colors for test

--- src/components/About.js
+++ src/components/About.js
@@ -1,22 +1,66 @@
 // components/About.js
  
 import React from 'react';
-import Navbar from './Navbar';
+// import Navbar from './Navbar';
 
-const about = () => {
+
+
+const colors = [
+  {
+    name: 'gray-800',
+    hex: '#227dc6',
+    rgb: '34 / 125 / 198',
+    cymk:'100 / 35 / 0 / 0',
+},
+{
+  name: 'gray-700',
+  hex: '#227dc6',
+  rgb: '34 / 125 / 198',
+  cymk:'100 / 35 / 0 / 0',
+},
+{
+  name: 'gray-600',
+  hex: '#227dc6',
+  rgb: '34 / 125 / 198',
+  cymk:'100 / 35 / 0 / 0',
+},
+{
+  name: 'gray-500',
+  hex: '#227dc6',
+  rgb: '34 / 125 / 198',
+  cymk:'100 / 35 / 0 / 0',
+},
+{
+  name: 'gray-400',
+  hex: '#227dc6',
+  rgb: '34 / 125 / 198',
+  cymk:'100 / 35 / 0 / 0',
+},
+{
+  name: 'gray-300',
+  hex: '#227dc6',
+  rgb: '34 / 125 / 198',
+  cymk:'100 / 35 / 0 / 0',
+},
+]
+
+export default function about() {
+
   return (
-    <div>
-    <Navbar>
-      
-    </Navbar>
-      <div style={{width: '40%', float:"left"}}>
-        <h1>About formgroup</h1>
-      </div>
-      <div style={{width: '60%', float:"right"}}>
-        <h2>content</h2>
-      </div>
-    </div>
+    <ul className="grid grid-cols-2 gap-x-2 gap-y-2 sm:grid-cols-4 sm:gap-x-4 lg:grid-cols-6 xl:gap-x-8 m-5">
+      {colors.map((color) => (
+        <li key={color.name} className="relative">
+          <div className={"h-40 w-40 rounded-lg p-3 bg-" + (color.name)}>
+            <button type="button" className="absolute inset-0 focus:outline-none">
+              <span className="sr-only">View details for {color.hex}</span>
+            </button>
+          <p className="mt-2 block text-sm font-medium text-white truncate pointer-events-none pl-5">{color.name}</p>
+          <p className="mt-2 block text-sm font-medium text-white truncate pointer-events-none pl-5">{color.hex}</p>
+          <p className="mt-2 block text-sm font-medium text-white truncate pointer-events-none pl-5">{color.cymk}</p>
+          <p className="mt-2 block text-sm font-medium text-white truncate pointer-events-none pl-5">{color.rgb}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   )
 }
- 
-export default about;
--- src/components/Home.js
+++ src/components/Home.js
@@ -69,7 +69,7 @@ const home = () => {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="#"
+                    href="/"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black bg-white hover:bg-gray-100 md:py-4 md:text-lg md:px-10"
                   >
                     Valentia Flyer
@@ -77,7 +77,7 @@ const home = () => {
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
-                    href="#"
+                    href="/"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-fgValentia-200 hover:bg-Valentia-400 md:py-4 md:text-lg md:px-10"
                   >
                     Live demo
