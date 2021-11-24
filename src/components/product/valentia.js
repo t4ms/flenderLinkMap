7a8bbdd3a6248752b3e605509406a19713d52f23
ThSchweers <th.schweers@googlemail.com>
2021-11-24 22:59:59 +0100

    Content adds to Valentia

--- src/components/product/valentia.js
+++ src/components/product/valentia.js
@@ -6,6 +6,7 @@ import { CheckIcon, ThumbUpIcon, UserIcon } from "@heroicons/react/solid";
 
 import ValentiaIntro from "../../image/svg/Valentia_Intro.svg";
 import ValentiaCorn from "../../image/png/valentia_corn.png";
+import iconCorn from '../../image/svg/ICON_fg_corn.svg'
 
 const summary = [
   {
@@ -65,8 +66,10 @@ const summary = [
   },
   {
     id: 4,
-    contentDe: "Hohe Wirkstoffpotenz d.h. geringe Wirkstoffausbringung und Gewässerbelastung",
-    contentEn: "Hohe Wirkstoffpotenz d.h. geringe Wirkstoffausbringung und Gewässerbelastung",
+    contentDe:
+      "Hohe Wirkstoffpotenz d.h. geringe Wirkstoffausbringung und Gewässerbelastung",
+    contentEn:
+      "Hohe Wirkstoffpotenz d.h. geringe Wirkstoffausbringung und Gewässerbelastung",
     target: "",
     href: "",
     highlightDe: "",
@@ -87,6 +90,33 @@ const summary = [
   },
 ];
 
+const section3 = [
+  {
+    descriptionDe: "Blattaktiv und schnell regenfest",
+    descriptionEn: "Leaf active and fast rainfast",
+  },
+  {
+    descriptionDe: "Gezielt, schnell & sicher im Nachauflauf",
+    descriptionEn: "Leaf active and fast rainfast",
+  },
+  {
+    descriptionDe: "Solo stark gegen Kamille, Klette, Nachtschatten, Knöterich-Arten, Franzosenkraut u.a.",
+    descriptionEn: "Leaf active and fast rainfast",
+  },
+  {
+    descriptionDe: "Wirkungsentfaltung weitgehend unabhängig von Strahlungsintensität oder Wachsschicht ",
+    descriptionEn: "Leaf active and fast rainfast",
+  },
+  {
+    descriptionDe: "Terbuthylazin frei",
+    descriptionEn: "Leaf active and fast rainfast",
+  },
+  {
+    descriptionDe: "Die Komplettlösung: Valentia plus Mesotrione",
+    descriptionEn: "Leaf active and fast rainfast",
+  },
+];
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -101,7 +131,7 @@ export default function valentia(props) {
               <div className="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center">
                 <div className="lg:py-24">
                   <a
-                    href="/"
+                    href="#Valentia Corn"
                     className="inline-flex items-center text-white bg-black rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200"
                   >
                     <span className="px-3 py-0.5 mx-1 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-green-light rounded-full">
@@ -150,79 +180,154 @@ export default function valentia(props) {
           </div>
         </div>
       </section>
-      <section>
 
-      <div className="relative bg-white">
-      <div className="lg:absolute lg:inset-0">
-        <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
-          <img
-            className="h-56 w-full object-cover lg:absolute lg:h-full"
-            src={ValentiaCorn}
-            alt="Valentia Corn"
-          />
-        </div>
-      </div>
-      <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
-        <div className="lg:col-start-2 lg:pl-8">
-          <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
-            <h2 className="leading-6 text-green-medium font-semibold tracking-wide uppercase">Valentia</h2>
-            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            Auf einen Blick
-            </h3>
-            <div className="flow-root">
-          <ul className="mt-8">
-            {summary.map((event, eventIdx) => (
-              <li key={event.id}>
-                <div className="relative pb-8">
-                  {eventIdx !== summary.length - 1 ? (
-                    <span
-                      className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
-                      aria-hidden="true"
-                    />
-                  ) : null}
-                  <div className="relative flex space-x-3">
-                    <div>
-                      <span
-                        className={classNames(
-                          event.iconBackground,
-                          "h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white"
-                        )}
-                      >
-                        <event.icon
-                          className="h-5 w-5 text-white"
-                          aria-hidden="true"
-                        />
-                      </span>
-                    </div>
-                    <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
-                      <div>
-                        <p className="text-sm text-black">
-                          {props.lang==='de' ? event.contentDe : event.contentEn} 
-                          <a
-                            href={event.href}
-                            className="font-medium text-gray-900"
-                          >
-                            {event.target}
-                          </a>
-                        </p>
-                      </div>
-                      <div className="text-right font-bold text-sm whitespace-nowrap text-green-lighter">
-                        <p>{props.lang==='de' ? event.highlightDe : event.highlightEn} </p>
-                      </div>
-                    </div>
-                  </div>
+      <section>
+        <div className="relative bg-white">
+          <div className="lg:absolute lg:inset-0">
+            <div className="lg:absolute lg:inset-y-0 lg:left-0 lg:w-1/2">
+              <img
+                className="h-56 w-full object-cover lg:absolute lg:h-full"
+                src={ValentiaCorn}
+                alt="Valentia Corn"
+              />
+            </div>
+          </div>
+          <div className="relative pt-12 pb-16 px-4 sm:pt-16 sm:px-6 lg:px-8 lg:max-w-7xl lg:mx-auto lg:grid lg:grid-cols-2">
+            <div className="lg:col-start-2 lg:pl-8">
+              <div className="text-base max-w-prose mx-auto lg:max-w-lg lg:ml-auto lg:mr-0">
+                <h2 className="leading-6 text-green-medium font-semibold tracking-wide uppercase">
+                  Valentia
+                </h2>
+                <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                  Auf einen Blick
+                </h3>
+                <div className="flow-root">
+                  <ul className="mt-8">
+                    {summary.map((event, eventIdx) => (
+                      <li key={event.id}>
+                        <div className="relative pb-8">
+                          {eventIdx !== summary.length - 1 ? (
+                            <span
+                              className="absolute top-4 left-4 -ml-px h-full w-0.5 bg-gray-200"
+                              aria-hidden="true"
+                            />
+                          ) : null}
+                          <div className="relative flex space-x-3">
+                            <div>
+                              <span
+                                className={classNames(
+                                  event.iconBackground,
+                                  "h-8 w-8 rounded-full flex items-center justify-center ring-8 ring-white"
+                                )}
+                              >
+                                <event.icon
+                                  className="h-5 w-5 text-white"
+                                  aria-hidden="true"
+                                />
+                              </span>
+                            </div>
+                            <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
+                              <div>
+                                <p className="text-sm text-black">
+                                  {props.lang === "de"
+                                    ? event.contentDe
+                                    : event.contentEn}
+                                  <a
+                                    href={event.href}
+                                    className="font-medium text-gray-900"
+                                  >
+                                    {event.target}
+                                  </a>
+                                </p>
+                              </div>
+                              <div className="text-right font-bold text-sm whitespace-nowrap text-green-lighter">
+                                <p>
+                                  {props.lang === "de"
+                                    ? event.highlightDe
+                                    : event.highlightEn}{" "}
+                                </p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </section>
 
-      
-       
+      <section id='Valentia Corn'>
+        <div className="bg-white">
+          <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+            <div>
+            <a href="/" className="inline-flex items-center text-white bg-gray-100 rounded-full p-1 pr-2 sm:text-base lg:text-sm xl:text-base hover:text-gray-200">
+                <span className="px-3 py-0.5 mx-1 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-green-light rounded-full">
+                  {props.lang === "de" ? "Mais" : "Corn"}
+                </span>
+            </a>
+                
+              <h2 className="mt-2 text-base font-semibold text-green-light uppercase tracking-wide">
+              Valentia
+              </h2>
+              <p className="mt-2 text-3xl font-extrabold text-gray-900">
+              Einzigartig in Mais
+              </p>
+              <div class='grid place-items-center mt-6'>
+              <img alt='Corn' src={iconCorn} className='w-48'/>
+              </div>
+            </div>
+            <div className="mt-12 lg:mt-0 lg:col-span-2">
+              <dl className="space-y-10 sm:space-y-3 items-center sm:grid sm:grid-cols-2 sm:grid-rows-3 ">
+                {section3.map((content) => (
+                  <div key={content.descriptionEn} className="relative">
+                    <dt>
+                      <CheckIcon
+                        className="absolute h-6 w-6 text-green-500"
+                        aria-hidden="true"
+                      />
+                      <p className="ml-9 text-lg leading-6 font-medium text-gray-900">
+                        {props.lang==='de' ? content.descriptionDe : content.descriptionEn}
+                      </p>
+                    </dt>
+                    {/* <dd className="mt-2 ml-9 text-base text-gray-500">
+                    {props.lang==='de' ? content.descriptionEn : content.descriptionDe}
+                    </dd> */}
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section>
+      <div className="relative md:bg-green-medium p-12">
+      <div>
+            <h2 className="text-base text-white font-semibold tracking-wide uppercase">Valentia</h2>
+            <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-green-dark sm:text-4xl">
+            Die neue, flexible Lösung 
+            </h3>
+          </div>
+            <div className="lg:grid lg:grid-cols-2 lg:gap-6 mt-6">
+
+              <div className="prose prose-indigo prose-lg text-white lg:max-w-none">
+                <p>
+                Die Unkrautregulierung im Mais steht vor neuen Herausforderungen. Ein zentraler blattaktiver Wirkstoffe steht nicht mehr zur Verfügung und bekannte bodenaktive Wirkstoffe wie Terbuthylazin oder Metolachlor sind zunehmend, nicht nur in grundwassersensiblen Gebieten, unerwünscht. 
+                </p>
+            
+              </div>
+              <div className="mt-6 prose prose-indigo prose-lg text-white lg:mt-0">
+                <p>
+                In dieser Situation ist Valentia die neue, flexible Lösung sowohl für den Solo-Einsatz, als auch als Mischungspartner zur Komplettierung des Wirkungsspektrums anderer Produkte – Bromoxynil Ersatz!
+                </p>
+              </div>
+            </div>
+          </div>
+
       </section>
     </>
   );
